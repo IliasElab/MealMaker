09e8e51e982dc84cf66789e40d0b00d85b58f2b6
IliasElab <elab.ilias@gmail.com>
2021-11-29 22:14:27 +0100

    Navbar + Bootstrap + Icon

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const dbmanager = require('./database')
 
 app.set('view engine', 'ejs')
 
-app.use(express.static('public'));
+app.use(express.static(__dirname+ '/public'));
 app.use(express.urlencoded({ extended: false}))
 
 app.get('/', (req, res) => {
