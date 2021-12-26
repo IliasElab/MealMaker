3071236adad093284f9e7f16e3bb2a3db1ad0c2c
IliasElab <elab.ilias@gmail.com>
2021-12-26 23:45:31 +0100

    Home and App / Navigation

---
+++ mealmaker_react/src/pages/AllRecipes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AllRecipes = () => {
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AllRecipes;
---
+++ mealmaker_react/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const Home = () => {
+    return (
+        <div>
+            <Navigation />
+            <h3>Je suis le component Home</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam cum debitis et consequatur ad dignissimos sapiente totam illo amet pariatur inventore at, asperiores nesciunt repellat quibusdam tempore ut deleniti modi harum, dicta iure doloremque dolorem. Repellat pariatur ratione dolore magni aperiam et fugit odio iste ullam consequatur alias neque non veritatis beatae, numquam esse quas eius! Eaque quaerat ab, totam quia dignissimos necessitatibus iure fugiat commodi enim ex dolorum corrupti facilis, deleniti dolores molestiae assumenda libero aperiam maxime? Repellendus, quam.</p>
+        </div>
+    );
+};
+
+export default Home;
